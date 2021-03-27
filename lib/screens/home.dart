933fc82e4c53a30ae71873765c24387571894959
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-03-27 10:20:31 -0700

    iOS

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  Widget build(BuildContext context) {
+    
+  }
+}
