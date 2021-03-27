160f940be648e7f54bb3511132a94384a67b7dcc
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2021-03-27 13:39:46 -0700

    stats

--- journey/lib/screens/profile.dart
+++ journey/lib/screens/profile.dart
@@ -9,7 +9,27 @@ class ProfileView extends StatefulWidget {
 class _ProfileViewState extends State<ProfileView> {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Profile")),
+      appBar: AppBar(
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(24),
+        child: Text(
+          "Your most common mood this month so far is HAPPY with 5 instances",
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      ),
     );
   }
 }
