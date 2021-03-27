b817de78e6820913fb125dec6b31f72afe08ddfe
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-03-27 12:09:55 -0700

    Update home.dart

--- journey/lib/screens/home.dart
+++ journey/lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
+  static const routeName = '/home';
+
   @override
   _HomeViewState createState() => _HomeViewState();
 }
@@ -11,14 +13,61 @@ class _HomeViewState extends State<HomeView> {
 
     final mediaQuery = MediaQuery.of(context);
 
+    final newEntry = FloatingActionButton(
+      onPressed: () {},
+      child: const Icon(Icons.add),
+      backgroundColor: Colors.blue,
+    );
+
     final pageBody = Container(
       height: (mediaQuery.size.height -
           mediaQuery.padding.top),
-      child: Center(child: Text("Test")),
+      child: Column(
+        children: <Widget>[
+          Spacer(),
+
+          Container(
+            height: 50,
+            child: Text(
+              "Good Afternoon, User"
+            ),
+          ),
+
+          Spacer(),
+
+          Container(
+            height: 200,
+            child: Text(
+              "Reflection Question"
+            ),
+          ),
+
+          Spacer(),
+
+          Container(
+            height: 200,
+            child: Text(
+              "Spotify Recommendations"
+            )
+          ),
+
+          Spacer(),
+
+          Divider(
+            height: 20,
+            thickness: 5,
+            indent: 20,
+            endIndent: 20,
+          ),
+
+          Spacer(flex: 2),
+        ]
+      )
     );
 
     return Scaffold(
       body: pageBody,
+      floatingActionButton: newEntry,
     );
   }
 }
