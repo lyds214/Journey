87821ce1c12959d477c9c57fd444e7fcab01a3b9
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-03-27 13:42:18 -0700

    Created ListView for entries

--- journey/lib/screens/entries.dart
+++ journey/lib/screens/entries.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+// Temporary entry class
+class Entry {
+  DateTime date;
+  int mood;
+  int music;
+  String journal;
+  Entry(this.date, this.mood, this.music, this.journal);
+}
 
 class EntryView extends StatefulWidget {
   static const routeName = '/Entry';
@@ -7,9 +17,144 @@ class EntryView extends StatefulWidget {
 }
 
 class _EntryViewState extends State<EntryView> {
+
+  // Moods: 1 worst, 5 best
+  // Music: 1 happy, 2 sad, 3 angry, 4 calm
+
+  List<Entry> _demo = [
+    Entry(DateTime.now(), 1, 3, "Terrible"),
+    Entry(DateTime.now(), 2, 2, "Bad"),
+    Entry(DateTime.now(), 3, 4, "Neutral"),
+    Entry(DateTime.now(), 3, 4, "Okay"),
+    Entry(DateTime.now(), 5, 1, "WOW IM SO HAPPY! The new Attack on Titan episode came out!")
+  ];
+
   Widget build(BuildContext context) {
+    
+    final mediaQuery = MediaQuery.of(context);
+
+    final pageBody = Container(
+      height: (mediaQuery.size.height -
+          mediaQuery.padding.top),
+      width: mediaQuery.size.width,
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: mediaQuery.padding.top),
+          Container(
+            height: 50,
+            width: mediaQuery.size.width,
+            child: Text(
+              "Entries",
+              textAlign: TextAlign.left,
+              style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0)
+            ),
+          ),
+          Container(
+            height: (mediaQuery.size.height -
+          mediaQuery.padding.top - ),
+            child: ListView.builder(
+              itemCount: _demo.length,
+              itemBuilder: (context, index) {
+                return EntryCard(_demo[index]);
+              }
+            ),
+          ),
+        ],
+      ),
+    );
+
     return Scaffold(
-      body: Center(child: Text("Entry")),
+      body: pageBody,
+    );
+  }
+
+  Widget EntryCard(Entry item) {
+    return Ink(
+      padding: const EdgeInsets.only(),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border(
+          top: BorderSide(
+            width: 1,
+            color: Colors.grey[300],
+          ),
+          bottom: BorderSide(
+            width: 1,
+            color: Colors.grey[300],
+          ),
+          left: BorderSide(
+            width: 1,
+            color: Colors.grey[300],
+          ),
+          right: BorderSide(
+            width: 1,
+            color: Colors.grey[300],
+          ),
+        ),
+      ),
+      child: InkWell(
+        onTap: () {},
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        height: 25,
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Text(
+                          "Mood: ${item.mood}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      Spacer(),
+                      Container(
+                        padding: const EdgeInsets.only(top: 5),
+                        alignment: Alignment.centerRight,
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '${DateFormat.yMMMd().format(item.date)}',
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 5),
+                        height: 25,
+                        width: 25,
+                        child: Center(
+                            child: Icon(Icons.arrow_forward_ios,
+                                color: Colors.grey, size: 15)),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    height: 30,
+                    padding: const EdgeInsets.only(bottom: 5, right: 5),
+                    child: Text(
+                        (item.journal.length > 35)
+                            ? '${item.journal.substring(0, 35)}...'
+                            : '${item.journal}',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(color: Colors.black45, fontSize: 15)),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 15),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
