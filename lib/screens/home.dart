66e9b49c199362c31f2dc8e197eb17d1c91d6997
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-03-27 10:35:20 -0700

    Work on home & tabs

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,7 +6,19 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+
   Widget build(BuildContext context) {
-    
+
+    final mediaQuery = MediaQuery.of(context);
+
+    final pageBody = Container(
+      height: (mediaQuery.size.height -
+          mediaQuery.padding.top),
+      child: Center(child: Text("Test")),
+    );
+
+    return Scaffold(
+      body: pageBody,
+    );
   }
 }
---
+++ lib/screens/tabs.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Tabs extends StatefulWidget {
+  static const routeName = '/tabs';
+
+  @override
+  _TabsState createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+
+  final List
+
+  // Pages
+  @override
+  void _initState() {
+    _pages =[]
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    )
+  }
+}
