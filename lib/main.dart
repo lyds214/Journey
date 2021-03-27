f2a332b82ecb20d487198a149e4306c1161f3cff
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-03-27 10:35:10 -0700

    Reroute main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// Views
+import './screens/home.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Journey',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: HomeView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
