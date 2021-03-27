a9a143bd8f2090c2fe1923a67d2f43865d0e871c
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-03-27 13:57:09 -0700

    Update main.dart
    Co-Authored-By: Lydia Yang <64216620+lyds214@users.noreply.github.com>

--- journey/lib/main.dart
+++ journey/lib/main.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Views
+import './screens/tabs.dart';
+import './screens/home.dart';
+import './onboarding/onboarding_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Journey());
 }
 
-class MyApp extends StatelessWidget {
+class Journey extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter'),
+      title: 'Journey',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(color: Color(0xFF4A14C)),
+        scaffoldBackgroundColor: Colors.purple[900],
+        textTheme: GoogleFonts.openSansTextTheme(
+          TextTheme(
+            bodyText1: TextStyle(),
+            bodyText2: TextStyle(),
+          ).apply(
+            bodyColor: Colors.white,
+            displayColor: Colors.blue,
           ),
-        ));
+        ),
+      ),
+      routes: {
+        Tabs.routeName: (ctx) => Tabs(),
+        HomeView.routeName: (ctx) => HomeView(),
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => Tabs());
+      },
+      home: Tabs(),
+    );
   }
 }
