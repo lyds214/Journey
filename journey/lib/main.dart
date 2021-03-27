795ed315ba41f90f422cfdf025b80252e6ad44d6
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-03-27 12:40:11 -0700

    Commit

--- journey/lib/main.dart
+++ journey/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Views
+import './screens/tabs.dart';
+import './onboarding/onboarding_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,18 +16,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.purple[900],
+        // textTheme: TextTheme(
+        //   bodyText1: TextStyle(),
+        //   bodyText2: TextStyle(),
+        // ).apply(
+        //   bodyColor: Colors.white, 
+        //   displayColor: Colors.blue,
+        // ),
+        textTheme: GoogleFonts.openSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Tabs(),
     );
   }
 }
--- journey/lib/screens/tabs.dart
+++ journey/lib/screens/tabs.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 // Views
 import 'home.dart';
